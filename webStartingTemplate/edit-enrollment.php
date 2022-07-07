209c0e6efe16d5d54dc181cb616e11cba85645c9
samirhashim <yummyhashim@gmail.com>
2022-07-07 13:45:48 +0300

    FEAT student.php

--- webStartingTemplate/edit-enrollment.php
+++ webStartingTemplate/edit-enrollment.php
@@ -5,6 +5,8 @@ $queryuser = mysqli_query($conn, "SELECT * FROM students WHERE id='".$_GET['id']
 while($fetchuser = mysqli_fetch_array($queryuser))
 {
 	$fullname = $fetchuser['fullname'];
+	$phonenumber = $fetchuser['phonenumber'];
+	$email = $fetchuser['email'];
 }
 ?>
 
@@ -24,9 +26,56 @@ while($fetchuser = mysqli_fetch_array($queryuser))
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header bg-dark text-center text-white">
-                            <h4>Edit student: <?php echo $fullname?></h4>
+                            <h4>Edit student: </h4>
+                        </div>
+						<div class="card-body">
+						<form action="enroll.php" method="POST">
+                    <div class="row">
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="fullname" class="form-label">Full name</label>
+                            <input type="text" class="form-control" name="fullname"value="<?php echo $fullname?>" placeholder="Enter your full name">
+                        </div>
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="phonenumber" class="form-label">Phone number</label>
+                            <input type="tel" class="form-control"name="phonenumber"value="<?php $phonenumber?>" placeholder="+2547...">
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" name="email"value="<?php $email?>" placeholder="Please enter your email">
+                        </div>
+                        <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                            <label for="gender" class="form-label">What's your gender</label>
+                            <select name="gender" class="form-control" aria-label="default select example">
+                                <option >--Select your gender--</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                        
+                        <div class="mb-3 col-lg-6">
+                            <select name="course" class="form-control multiplchose_questiontypes" id="selector">
+                                <option value="" >select course--</option>
+                                <option value="web design">web design</option>
+                                <option value="cyber security">cyber security</option>
+                                <option value="android development">android development</option>
+
+                            </select>
+                         </div>
+						
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                        <label class="form-check-label" for="flexCheckChecked">
+                           Agree terms and condition
+                        </label>
+                        </div>
+                    </div>           
+                 <button type="submit" name="enroll" class="btn btn-outline-primary">Update Records</button>
+            </div>
+        </form>
+		    </div>
+                </div>
 
                 </div>
             </div>
